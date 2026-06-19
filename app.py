143b782325b14c697a7c8246239b70533de9691d
guechanlee <guechanlee@gmail.com>
2026-06-19 17:58:01 +0900

    충돌 병합

--- app.py
+++ app.py
@@ -1,38 +1,18 @@
 from flask import Flask, render_template
-<<<<<<< HEAD
-<<<<<<< HEAD
-from blueprints.member.routes import member_bp
-from blueprints.bank.routes import bank_bp
-=======
 from blueprints.account.routes import account_bp
->>>>>>> d71108042a4a7803b5f684ed0fd5d46ced29197f
-=======
-<<<<<<< Updated upstream
-from blueprints.account.routes import account_bp
-=======
 from blueprints.diary.routes import diary_bp
-from blueprints.member.routes import member_bp
->>>>>>> Stashed changes
->>>>>>> 265692cbf4ff708769f1aec922e217c130c2955c
-
+from blueprints.memo.routes import memo_bp
+from blueprints.bank.routes import bank_bp
 # essentials
 app = Flask(__name__)
 app.secret_key = "KLASFkjduiqw18asdBCbnkpq-2098418"
 
 # bluprints
-<<<<<<< Updated upstream
+app.register_blueprint(memo_bp)
 app.register_blueprint(account_bp)
-
-<<<<<<< HEAD
-# bank
+app.register_blueprint(diary_bp)
 app.register_blueprint(bank_bp)
 
-=======
-=======
-app.register_blueprint(member_bp)
-app.register_blueprint(diary_bp)
->>>>>>> Stashed changes
->>>>>>> 265692cbf4ff708769f1aec922e217c130c2955c
 
 # entrypoint
 @app.route("/", methods=["GET"])

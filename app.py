8b3c8a7270399c2a556dac08c5c250dd7ae7aee2
guechanlee <guechanlee@gmail.com>
2026-06-19 17:39:43 +0900

    20260619commit

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+<<<<<<< Updated upstream
 from blueprints.account.routes import account_bp
+=======
+from blueprints.diary.routes import diary_bp
+from blueprints.member.routes import member_bp
+>>>>>>> Stashed changes
 
 # essentials
 app = Flask(__name__)
 app.secret_key = "KLASFkjduiqw18asdBCbnkpq-2098418"
 
 # bluprints
+<<<<<<< Updated upstream
 app.register_blueprint(account_bp)
 
+=======
+app.register_blueprint(member_bp)
+app.register_blueprint(diary_bp)
+>>>>>>> Stashed changes
 
 # entrypoint
 @app.route("/", methods=["GET"])

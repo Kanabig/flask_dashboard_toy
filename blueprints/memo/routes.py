e3a0773b2925debc71fe88bcb16b13a82cb55942
mayphyton112 <mayphyton112@gmail.com>
2026-06-20 23:15:07 +0900

    20260620 전체 라우터 구성 및 메뉴틀 잡기 by ktj 23:15

--- blueprints/memo/routes.py
+++ blueprints/memo/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, session
+from utils.json_manager import load_json, save_json, ACCOUNT_FILE
+
 
 memo_bp = Blueprint(
     'memo',
@@ -11,7 +13,20 @@ memo_bp = Blueprint(
 def memo_main():
     return render_template("memo/memo_Service.html")
 
-# /memo/create/
-# /memo/read/
 # /memo/write/
-# /memo/delete
+@memo_bp.route("/write", methods=['GET'])
+def memo_write():
+    return render_template("memo/memo_Write.html")
+    
+# /memo/read/
+@memo_bp.route("/read", methods=['GET'])
+def memo_read():
+    return render_template("memo/memo_Read.html")
+# /memo/write/
+@memo_bp.route("/update", methods=['GET'])
+def memo_update():
+    return render_template("memo/memo_Update.html")
+# /memo/delete/
+@memo_bp.route("/delete", methods=['GET'])
+def memo_delete():
+    return render_template("memo/memo_Delete.html")

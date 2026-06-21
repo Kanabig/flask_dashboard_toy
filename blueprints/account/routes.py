f82ddb8fc725ebeda08c4a970ee8c5533e64eb32
Kanabig <parkjungho20@gmail.com>
2026-06-21 23:19:51 +0900

    feat: account service complete

--- blueprints/account/routes.py
+++ blueprints/account/routes.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, session
-from utils.json_manager import load_json, save_json, ACCOUNT_FILE
-from utils.timestamper import get_current_time_stamp_formated
+from flask import Blueprint, render_template, request, redirect, session, url_for
+from blueprints.account import access
 
 account_bp = Blueprint(
     "account",
@@ -8,23 +7,33 @@ account_bp = Blueprint(
     url_prefix="/account",
 )
 
-KEY_ID = "id"
-KEY_PW = "pw"
-KEY_MAIL = "mail"
-KEY_PHONE = "phone"
-KEY_REG_DATE = "reg_date"
-KEY_MOD_DATE = "mod_date"
 KEY_SESSION_ID = "signinedMemberId"
 
 
-# account main --------------------------------------------------------------
+# --- utils ---
+def get_signed_in_account_id():
+    return session.get(KEY_SESSION_ID)
+
+
+def require_signed_in_account():
+    account_id = get_signed_in_account_id()
+    if not account_id:
+        return None, redirect(url_for("account.signin_form"))
+    return account_id, None
+
+
+def get_form_value(key):
+    return request.form.get(key, "").strip()
+
+
+# --- ACCOUNT SERVICE ROOT ---
 @account_bp.route("/", methods=["GET"])
 def account_service_main():
-    id = session.get("signinedMemberId")
-    return render_template("account/account_main.html", id=id)
+    account_id = get_signed_in_account_id()
+    return render_template("account/account_main.html", id=account_id)
 
 
-# signup --------------------------------------------------------------------
+# --- SIGN UP ---
 @account_bp.route("/signup_form", methods=["GET"])
 def signup_form():
     return render_template("account/signup_form.html")
@@ -32,31 +41,19 @@ def signup_form():
 
 @account_bp.route("/signup_confirm", methods=["POST"])
 def signup_confirm():
-    id = request.form["mId"]
-    pw = request.form["mPw"]
-    mail = request.form["mMail"]
-    phone = request.form["mPhone"]
+    id = get_form_value("mId")
+    password = get_form_value("mPw")
+    mail = get_form_value("mMail")
+    phone = get_form_value("mPhone")
 
-    account = load_json(ACCOUNT_FILE)
-
-    if id in account:
-        return render_template("account/signup_result.html", result="NG")
-
-    account[id] = {
-        KEY_ID: id,
-        KEY_PW: pw,
-        KEY_MAIL: mail,
-        KEY_PHONE: phone,
-        KEY_REG_DATE: get_current_time_stamp_formated(),
-        KEY_MOD_DATE: get_current_time_stamp_formated(),
-    }
-
-    save_json(ACCOUNT_FILE, account)
-
-    return render_template("account/signup_result.html", result="OK")
+    result = access.sign_up(id, password, mail, phone)
+    return render_template(
+        "account/signup_result.html",
+        result=result,
+    )
 
 
-# signin --------------------------------------------------------------------
+# --- SIGN IN ---
 @account_bp.route("/signin_form", methods=["GET"])
 def signin_form():
     result = request.args.get("result")
@@ -65,21 +62,62 @@ def signin_form():
 
 @account_bp.route("/signin_confirm", methods=["POST"])
 def signin_confirm():
-    id = request.form["mId"]
-    pw = request.form["mPw"]
+    id = get_form_value("mId")
+    password = get_form_value("mPw")
 
-    members = load_json(ACCOUNT_FILE)
-
-    if id in members and members[id][KEY_PW] == pw:
+    if access.sign_in(id, password):
         session[KEY_SESSION_ID] = id
-        return redirect("/account/")
-        # return render_template("/account/signin_result.html")
+        return redirect(url_for("account.account_service_main"))
 
-    return redirect("/account/signin_form?result=fail")
+    return redirect(url_for("account.signin_form", result="False"))
 
 
-# signout -------------------------------------------------------------------
+# --- SIGN OUT ---
 @account_bp.route("/signout_confirm", methods=["GET"])
 def signout_confirm():
     session.clear()
-    return redirect("/")
+    return redirect(url_for("main_page"))
+
+
+# --- DELETE ACCOUNT ---
+@account_bp.route("/delete_confirm", methods=["GET", "POST"])
+def delete_confirm():
+    id, redirect_response = require_signed_in_account()
+    if redirect_response:
+        return redirect_response
+
+    if request.method == "POST":
+        if access.remove_account_by_id(id):
+            session.clear()
+        return redirect(url_for("main_page"))
+
+    return render_template("account/delete_confirm.html", id=id)
+
+
+# --- MODIFY ACCOUNT ---
+@account_bp.route("/modify_form", methods=["GET"])
+def modify_form():
+    id, redirect_response = require_signed_in_account()
+    if redirect_response:
+        return redirect_response
+
+    account = access.get_account_by_id(id)
+    if not account:
+        session.clear()
+        return redirect(url_for("account.signin_form"))
+
+    return render_template("account/modify_form.html", account=account)
+
+
+@account_bp.route("/modify_confirm", methods=["POST"])
+def modify_confirm():
+    id, redirect_response = require_signed_in_account()
+    if redirect_response:
+        return redirect_response
+
+    password = get_form_value("mPw")
+    mail = get_form_value("mMail")
+    phone = get_form_value("mPhone")
+
+    access.modify_account(id, password, mail, phone)
+    return redirect(url_for("account.account_service_main"))
